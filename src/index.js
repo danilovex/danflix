2a49d94190b9215f17c9058302758ba258ca3457
dramalho <dramalho@NOTEIMG459>
2020-08-01 10:44:57 -0300

    ultima aula

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import './index.css';
-import Home from './pages/Home';
-import CadastroVideo from './pages/cadastro/Video'
-import CadastroCategoria from './pages/cadastro/Categoria'
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
   <BrowserRouter>
-  <Switch>
-    <Route path="/cadastro/video" component={CadastroVideo} exact />
-    <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
-    <Route path="/" component={Home} exact />
-    <Route component={() => (<div>Página 404</div>)} />
-    <Home />
-  </Switch>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
-
